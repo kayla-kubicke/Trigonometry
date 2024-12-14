952e5ae4a5d087654ad0fc401b854222f433b67f
kayla-kubicke <161649341+kayla-kubicke@users.noreply.github.com>
2024-12-14 09:21:11 -0800

    GeneralTriangle Refactor
    Defined skeleton method responsibilites.

--- Triangle.java
+++ Triangle.java
@@ -2,31 +2,52 @@
 * The GeneralTriangle interface contains skeleton methods
 * for all Triangle objects.
 */
-public interface GeneralTriangle {
-	// Single triangle Boolean methods
-	public boolean isRight();
-	public boolean isAcute();
-	public boolean isEquilateral();
-	public boolean isObtuse();
-	public boolean isIsoceles();
 
+public interface GeneralTriangle {
+	//
+	// Type methods will contain logic to determine type.
+	public boolean isType(); // Unnecessary?
+	public Triangle getType();
+		// REFACTOR: Collapse into two method.
+		// public boolean isRight();
+		// public boolean isAcute();
+		// public boolean isEquilateral();
+		// public boolean isObtuse();
+		// public boolean isIsoceles();
+
+
+	//
+	// Possible triangle methods will contain logic to determine if
+	// proposed triangle is possible.
+	public boolean isPossibleTriangle(Side sideA, Side sideB, Side sideC); // Triangle Inequality Theorem.
+	public boolean isPossibleTriangle(Angle angleA, Angle angleB, Angle angleC); // Equals 180 degrees.
+
+
+	//
 	// Triangle comparison Boolean method
 	public boolean areSimilar(Triangle comparisonTriangle);
 
 
+	//
 	// Informational (get) methods
-	public double getSideLengthA();
-	public double getSideLengthB();
-	public double getSideLengthC();
+	public double getSideLength(Side side); // Build Side object?
+		// REFACTOR: Collapse into one method.
+		// public double getSideLengthA();
+		// public double getSideLengthB();
+		// public double getSideLengthC();
 
-	public double getAngleAlpha();
-	public double getAngleBeta();
-	public double getAngleGamma();
+	public double getAngle(Angle angle); // Build Angle object?
+		// REFACTOR: Collapse into one method.
+		// public double getAngleAlpha();
+		// public double getAngleBeta();
+		// public double getAngleGamma();
 
-	public String getInformation();
+	public String getInformation(); // Just use toString()?
 	public String getUnknownInformation();
 	public String getKnownInformation();
 
+	//
+	// Calculation methods
 	public double getArea();
 	public double getPerimeter();
 }
